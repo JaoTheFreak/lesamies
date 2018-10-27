004caad80c82e3c52eea8355fef4dfcaaa7c5616
João Antonio Martins Filho <joaoantoniomartinsfilho@gmail.com>
2018-10-27 18:39:36 -0300

    Projetos

---
+++ Data.LesAmies/Contexts/LesAmiesContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.LesAmies.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.LesAmies.Contexts
+{
+    public class LesAmiesContext : DbContext
+    {
+        public LesAmiesContext(DbContextOptions<LesAmiesContext> options)
+            : base(options)
+        {
+            
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
